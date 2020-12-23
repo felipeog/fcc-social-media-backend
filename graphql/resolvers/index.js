fdcadc1e74a54a14789129c74a88a3cbbcb9e5a9
felipeog <felipeog476@gmail.com>
2020-12-23 00:21:32 -0300

    Add like and comment count

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -3,6 +3,10 @@ const usersResolvers = require('./users')
 const commentsResolvers = require('./comments')
 
 module.exports = {
+  Post: {
+    likeCount: ({ likes }) => likes.length,
+    commentCount: ({ comments }) => comments.length,
+  },
   Query: {
     ...postsResolvers.Query,
   },
