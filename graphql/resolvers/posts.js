10c6eca2a252ab332e6ff07e93f52b84bfc0b067
felipeog <felipeog476@gmail.com>
2020-12-23 00:12:15 -0300

    Add likePost mutation

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -1,4 +1,4 @@
-const { AuthenticationError } = require('apollo-server')
+const { AuthenticationError, UserInputError } = require('apollo-server')
 
 const Post = require('../../models/Post')
 const checkAuth = require('../../utils/checkAuth')
@@ -36,9 +36,13 @@ module.exports = {
         username: user.username,
         createdAt: new Date().toISOString(),
       })
-      const post = await newPost.save()
 
-      return post
+      try {
+        const post = await newPost.save()
+        return post
+      } catch (err) {
+        throw new Error(err)
+      }
     },
     deletePost: async (_, { postId }, context) => {
       const user = checkAuth(context)
@@ -50,6 +54,27 @@ module.exports = {
           return 'Post deleted'
         } else {
           throw new AuthenticationError('Action not allowed')
+        }
+      } catch (err) {
+        throw new Error(err)
+      }
+    },
+    likePost: async (_, { postId }, context) => {
+      const { username } = checkAuth(context)
+
+      try {
+        const post = await Post.findById(postId)
+        if (post) {
+          if (post.likes.find((like) => like.username === username)) {
+            post.likes = post.likes.filter((like) => like.username !== username)
+          } else {
+            post.likes.push({ username, createdAt: new Date().toISOString() })
+          }
+
+          await post.save()
+          return post
+        } else {
+          throw new UserInputError('Post not found')
         }
       } catch (err) {
         throw new Error(err)
