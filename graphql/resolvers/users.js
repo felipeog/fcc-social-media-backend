440e1a2cf79449d56258dc36f147fa63cf261545
felipeog <felipeog476@gmail.com>
2020-12-23 00:11:48 -0300

    Add try catch

--- graphql/resolvers/users.js
+++ graphql/resolvers/users.js
@@ -24,29 +24,33 @@ module.exports = {
         throw new UserInputError('Errors', { errors })
       }
 
-      const user = await User.findOne({ username })
-      if (user) {
-        throw new UserInputError('Username is taken', {
-          errors: {
-            username: 'This username is taken',
-          },
+      try {
+        const user = await User.findOne({ username })
+        if (user) {
+          throw new UserInputError('Username is taken', {
+            errors: {
+              username: 'This username is taken',
+            },
+          })
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 12)
+        const newUser = new User({
+          email,
+          username,
+          password: hashedPassword,
+          createdAt: new Date().toISOString(),
         })
-      }
+        const res = await newUser.save()
+        const token = generateToken(res)
 
-      const hashedPassword = await bcrypt.hash(password, 12)
-      const newUser = new User({
-        email,
-        username,
-        password: hashedPassword,
-        createdAt: new Date().toISOString(),
-      })
-      const res = await newUser.save()
-      const token = generateToken(res)
-
-      return {
-        ...res._doc,
-        id: res._id,
-        token,
+        return {
+          ...res._doc,
+          id: res._id,
+          token,
+        }
+      } catch (err) {
+        throw new Error(err)
       }
     },
 
@@ -59,24 +63,26 @@ module.exports = {
         throw new UserInputError('Errors', { errors })
       }
 
-      const user = await User.findOne({ username })
-      if (!user) {
-        errors.general = 'User not found'
-        throw new UserInputError('User not found', { errors })
-      }
+      try {
+        const user = await User.findOne({ username })
+        if (!user) {
+          errors.general = 'User not found'
+          throw new UserInputError('User not found', { errors })
+        }
 
-      const match = await bcrypt.compare(password, user.password)
-      if (!match) {
-        errors.general = 'Wrong credentials'
-        throw new UserInputError('Wrong credentials', { errors })
-      }
+        const match = await bcrypt.compare(password, user.password)
+        if (!match) {
+          errors.general = 'Wrong credentials'
+          throw new UserInputError('Wrong credentials', { errors })
+        }
 
-      const token = generateToken(user)
-
-      return {
-        ...user._doc,
-        id: user._id,
-        token,
+        return {
+          ...user._doc,
+          id: user._id,
+          token: generateToken(user),
+        }
+      } catch (err) {
+        throw new Error(err)
       }
     },
   },
