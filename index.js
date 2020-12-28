6989868b47fcba342c548bb199fea3be17436b7f
felipeog <felipeog476@gmail.com>
2020-12-28 20:21:43 -0300

    Add cors origin option

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ const server = new ApolloServer({
   typeDefs: require('./graphql/typeDefs'),
   resolvers: require('./graphql/resolvers'),
   context: ({ req }) => ({ req }),
+  cors: {
+    origin: process.env.CORS_ORIGIN || true,
+  },
 })
 
 mongoose
